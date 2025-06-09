c1118ae816b4e794ed587cf27595e9e4e88ff401
RamyBoulos <ramyboulos@gmail.com>
2025-06-09 12:19:29 +0200

    Update config and .gitignore, add sample_creator notebook

--- src/config.py
+++ src/config.py
@@ -10,9 +10,18 @@ SAMPLED_DATA_DIR = os.path.join(PROJECT_ROOT, "data", "sampled")
 
 # File paths
 TRAIN_LABELS_PATH = os.path.join(RAW_DATA_DIR, "train_labels.csv")
+
 SAMPLED_LABELS_PATH = os.path.join(SAMPLED_DATA_DIR, "sampled_train_labels.csv")
 
+SAMPLED_TRAIN_DIR = os.path.join(SAMPLED_DATA_DIR, "sampled_train")
+
+# File path for full labels CSV (from original dataset, not uploaded to GitHub)
+FULL_LABELS_PATH = os.path.join(RAW_DATA_DIR, "train_labels.csv")
+
 # Directory for full training tomograms (each tomo_id folder with .jpg slices)
-# ⚠️ NOTE: This folder is not uploaded to the GitHub repository due to its large size.
+# NOTE: This folder is not uploaded to the GitHub repository due to its large size.
 #         You must manually set this path according to your local setup if needed.
 FULL_DATA_TRAIN_DIR = os.path.join(PROJECT_ROOT, "..", "byu-locating-bacterial-flagellar-motors-2025", "train")
+
+# Directory for externally stored sampled training images
+EXTERNAL_SAMPLED_TRAIN_DIR = os.path.join(PROJECT_ROOT, "data", "sampled", "sampled_train")
